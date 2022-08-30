5de3a2f6cdfb1de89c548c626dd6dd2c35db0898
Biraj Pantha <birajpanta112@gmail.com>
2022-08-30 15:15:26 +0100

    2nd commit

--- Wallpaper_downloader.py
+++ Wallpaper_downloader.py
@@ -3,14 +3,14 @@ import requests
 def WallpaperSearh(SearchQuery):
     url =  f"https://wallhave.cc/api/v1/search?={SearchQuery}"
     rData = requests.get(url) #grabs the REST api data. 
-    json_data = rData.json() # returns a json object of the result.
+    JsonData = rData.json() # returns a json object of the result.
     downloadLinks = []
-    for item in json_data["data"]: # for each wallpaper(object) in the 'data' list 
+    for item in JsonData["data"]: # for each wallpaper(object) in the 'data' list 
         downloadLinks.append(item["path"])
 
     return downloadLinks #returns a list of wallpaper jpeg links
 
-downloadURL = WallpaperSearh("anime")
+downloadURL = WallpaperSearh("minamal")
 
 for url in downloadURL:
     print(url)
